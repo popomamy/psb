8a155bf352aa564de59cd6cc8138d0b7758e3753
popomamy <ahmadmraihan699@gmail.com>
2021-12-19 21:06:28 +0700

    initial commit

---
+++ database/migrations/2021_12_19_075330_create_forms_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->id();
+            $table->string('nisn')->unique();
+            $table->string('jenis_kelamin');
+            $table->date('ttl');
+            $table->string('agama');
+            $table->string('nama_ortu_wali');
+            $table->string('pekerjaan_ortu_wali');
+            $table->string('alamat');
+            $table->string('kp');
+            $table->string('rt_rw');
+            $table->string('kecamatan');
+            $table->string('kab_kota');
+            $table->string('no_telp');
+            $table->string('asal_sekolah');
+            $table->string('nilai_mtk');
+            $table->string('nilai_indo');
+            $table->string('nilai_ing');
+            $table->string('nilai_ipa');
+            $table->string('ijazah_skhun');
+            $table->string('akta');
+            $table->string('kk');
+            $table->string('status');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('major_id')->references('id')->on('majors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <title>PPDB</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark a sticky-top">
+        <div class="container">
+          <a class="navbar-brand" href="#"><img src="img/logo.png" width="230px" alt=""></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+           
+            </ul>
+            <form class="d-flex">
+              <button class="main-btn" type="submit">Masuk</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <div class="container-fluid top-banner">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <h2 class="jarak">PPDB</h2>
+                <p class="jarak2">Penerimaan Peserta Didik baru</p>
+                <h3 class="jarak3">SMAN 3 TASIKMALAYA</h3>
+              </div>
+              <div class="col">
+                <div class="card jarakcard shadow-lg">
+                  <div class="card-body colcard">
+                  <h3 class="text-center">FORM PENDAFTARAN</h3>
+                  <div class="mt-5 mb-2 ms-3 row">
+                    <label class="col-sm-4 col-form-label">Nama Lengkap</label>
+                    <div class="col-sm-7">
+                      <input type="text" class="form-control" id="inputPassword">
+                    </div>
+                  </div>
+                  <div class="mb-2 ms-3 row">
+                    <label class="col-sm-4 col-form-label">Email</label>
+                    <div class="col-sm-7">
+                      <input type="email" class="form-control" id="inputPassword">
+                    </div>
+                  </div>
+                  <div class="mb-2 ms-3 row">
+                    <label class="col-sm-4 col-form-label">Password</label>
+                    <div class="col-sm-7">
+                      <input type="password" class="form-control" id="inputPassword">
+                    </div>
+                  </div>
+                  <div class="mb-2 mt-4 row ">
+                    <label class="col-sm-8 col-form-label"></label>
+                    <div class="col-sm-4">
+                      <button class="main-btn2" type="submit">Daftar</button>
+                    </div>
+                  </div>
+                  <div class="mb-2 ms-3 row">
+                    <label class="col-sm-7 col-form-label">Sudah Mendaftar? <a href="#">Masuk</a></label>
+                    <div class="col-sm-5">
+                     
+                    </div>
+                  </div>
+                  </div>
+              </div>
+              </div>
+            </div>
+          </div>
+      </div>
+ 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  
+    
+  </body>
+</html>
